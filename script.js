e6fbf030e2682eef18e651a46af84dbc9b338419
amang10 <90174464+amang10@users.noreply.github.com>
2025-04-25 10:03:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const makeChange = (c) => {
-  // your name here
-};
+  const amount = Number(c);
+  const change = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+  change.q = Math.floor(amount / 25);
+  c %= 25;
+
+  change.d = Math.floor(c / 10);
+  c %= 10;
+
+  change.n = Math.floor(c / 5);
+  c %= 5;
+
+  change.p = c;
+
+  return change;
+};
